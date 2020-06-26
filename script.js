ef8874eff3bce72cbf81710cb79c59d7d80f3658
nosremetnarg <grant.alex.emerson@gmail.com>
2020-06-26 16:42:51 -0500

    buttons working restart working

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
@@ -6,6 +7,10 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
     console.log('started')
@@ -17,6 +22,7 @@ function startGame() {
 }
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
@@ -34,18 +40,89 @@ function showQuestion(question) {
     })
 }
 
-function selectAnswer(e) {
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
 
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex +1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+   
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    } 
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 const questions = [
     {
         question: 'Commonly used data types DO NOT include',
         answers: [
-            { text: 'numbers', correct: true },
-            { text: 'hotdogs', correct: false },
-            { text: 'booleans', correct: true },
-            { text: 'strings', correct: true },
+            { text: 'numbers', correct: false },
+            { text: 'hotdogs', correct: true },
+            { text: 'booleans', correct: false },
+            { text: 'strings', correct: false },
         ]
-    }
+    },
+    {
+        question: 'Who is the bassplayer in Red Hot Chili Peppers?',
+        answers: [
+            { text: 'ratguy', correct: false },
+            { text: 'tick', correct: false },
+            { text: 'sid vicious', correct: false },
+            { text: 'flea', correct: true },
+        ]
+    },
+    {
+        question: 'Who is the bassplayer in Incubus?',
+        answers: [
+            { text: 'Dirk Lance', correct: false },
+            { text: 'Ben Kenney', correct: true },
+            { text: 'Fred Durst', correct: false },
+            { text: 'Ziggy Stardust', correct: false },
+        ]
+    },
+    {
+        question: 'Is learning code easy?',
+        answers: [
+            { text: 'super easy', correct: false },
+            { text: 'no', correct: true },
+            { text: 'hell no', correct: true },
+            { text: 'its hard AF', correct: true },
+        ]
+    },
+    {
+        question: 'Are you voting in 2020?',
+        answers: [
+            { text: 'no', correct: false },
+            { text: 'i don\'t care', correct: false },
+            { text: 'hell no', correct: false },
+            { text: 'YES YES YES', correct: true },
+        ]
+    },
 ]
